d2f090a97050e1b865c911591bd848190ebde4c7
Nathannael Amaro <nathannael_g3@hotmail.com>
2025-05-09 10:14:13 -0300

    :recycle: refactor: Padronização de formulário na página login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,31 +4,33 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useAction } from "next-safe-action/hooks";
 import { login } from "@/actions/auth/login";
-import { useForm } from "react-hook-form"
-import { FormEvent } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
 import { toast } from "sonner";
 import { useRouter } from '@bprogress/next/app';
 
-type FormInputs = {
+type IFormInputs = {
     email: string,
     password: string
 }
 
 export default function LoginPage() {
     const loginAction = useAction(login);
-    const { register, getValues } = useForm<FormInputs>();
+    const { handleSubmit, control } = useForm<IFormInputs>({
+        mode: "onSubmit",
+        defaultValues: {
+            email: '',
+            password: '',
+        }
+    })
     const router = useRouter();
 
 
     // Função executada ao clicar no botão entrar
-    async function handleFormSubmit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault()
-
-        const formData = getValues()
+    const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
 
         const responseLoginAction = await loginAction.executeAsync({
-            email: formData.email,
-            password: formData.password
+            email: data.email,
+            password: data.password
         })
 
         // Verificando se retornou algum erro de validação na server action
@@ -54,22 +56,36 @@ export default function LoginPage() {
             </span>
 
             {/*FORM*/}
-            <form className="w-full flex flex-col justify-center items-center space-y-11" onSubmit={handleFormSubmit}>
+            <form className="w-full flex flex-col justify-center items-center space-y-11" onSubmit={handleSubmit(onSubmit)}>
                 {/*INPUTS*/}
                 <div className="w-xl flex flex-col space-y-4">
-                    <Input {...register("email")}
-                        variant="zinc900"
-                        placeholder="Digite seu email"
-                        icon="AtSign"
-                        errorsMessages={loginAction.result.validationErrors?.email?._errors}
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field }) => (
+                            <Input {...field}
+                                variant="zinc900"
+                                placeholder="Digite seu email"
+                                icon="AtSign"
+                                errorsMessages={loginAction.result.validationErrors?.email?._errors}
+                            />
+                        )}
                     />
-                    <Input {...register("password")}
-                        variant="zinc900"
-                        placeholder="Digite sua senha"
-                        icon="KeyRound"
-                        type="password"
-                        errorsMessages={loginAction.result.validationErrors?.password?._errors}
+
+                    <Controller
+                        control={control}
+                        name="password"
+                        render={({ field }) => (
+                            <Input {...field}
+                                variant="zinc900"
+                                placeholder="Digite sua senha"
+                                icon="KeyRound"
+                                type="password"
+                                errorsMessages={loginAction.result.validationErrors?.password?._errors}
+                            />
+                        )}
                     />
+
                     <Button type="button" variant="link" size="clean" className="text-zinc-300 text-xs">
                         Esqueci minha senha
                     </Button>
